e9796cc5217f7b56ed7afbe66e189d156a5c7c1c
Brian Hendriks <brian@liquidata.co>
2020-09-18 14:44:47 -0700

    updated test

--- bats/server_multiclient_test.py
+++ bats/server_multiclient_test.py
@@ -70,10 +70,10 @@ def create_branch(dc, branch_name):
 
 # work functions
 
-def connect(dc):
+def connect(dc, ctx):
     dc.connect()
 
-def create_tables(dc):
+def create_tables(dc, ctx):
     query(dc, 'SET @@repo1_head=HASHOF("master");')
     query(dc, """
 CREATE TABLE test (
@@ -84,7 +84,7 @@ PRIMARY KEY(pk));""")
     commit_and_update_branch(dc, "Created tables", ["@@repo1_head"], "master")
     query_and_test_results(dc, "SHOW TABLES;", [{"Table": "test"}])
 
-def duplicate_table_create(dc):
+def duplicate_table_create(dc, ctx):
     query(dc, 'SET @@repo1_head=HASHOF("master");')
     query_with_expected_error(dc, "Should have failed creating duplicate table", """
 CREATE TABLE test (
@@ -94,7 +94,7 @@ c2 INT,
 PRIMARY KEY(pk));""")
 
 
-def seed_master(dc):
+def seed_master(dc, ctx):
     query(dc, 'SET @@repo1_head=HASHOF("master");')
     _, row_count = query(dc, 'INSERT INTO test VALUES (0,0,0),(1,1,1),(2,2,2)')
 
@@ -105,50 +105,51 @@ def seed_master(dc):
     expected = [row(0,0,0), row(1,1,1), row(2,2,2)]
     query_and_test_results(dc, "SELECT pk, c1, c2 FROM test ORDER BY pk", expected)
 
-def modify_pk0_on_master_and_commit(dc):
-    query(dc, 'SET @@repo1_head=HASHOF("master");')
-    query(dc, "UPDATE test SET c1=1 WHERE pk=0;")
-    commit_and_update_branch(dc, "set c1 to 1", ["@@repo1_head"], "master")
+def race_to_commit1(dc, ctx):
+    race_to_commit(dc, ctx, 1, 2)
 
-def modify_pk0_on_master_no_commit(dc):
-    query(dc, 'SET @@repo1_head=HASHOF("master");')
-    query(dc, "UPDATE test SET c1=2 WHERE pk=0")
+def race_to_commit2(dc, ctx):
+    race_to_commit(dc, ctx, 2, 1)
 
-def fail_to_commit(dc):
+def race_to_commit(dc, ctx, my_val, their_val):
+    query(dc, 'SET @@repo1_head=HASHOF("master");')
+    query(dc, "UPDATE test SET c1=%d WHERE pk=0;" % my_val)
+
     try:
-        commit_and_update_branch(dc, "Created tables", ["@@repo1_head"], "master")
-        raise Exception("Failed to fail commit")
+        commit_and_update_branch(dc, "set c1 to 1", ["@@repo1_head"], "master")
+        ctx['race_result'] = "winner"
+
     except Exception as e:
-        if str(e) != UPDATE_BRANCH_FAIL_MSG:
-            raise e
-
-def commit_to_feature(dc):
-    create_branch(dc, "feature")
-    commit_and_update_branch(dc, "set c1 to 2", ["@@repo1_head"], "feature")
-
-def merge_resolve_commit(dc):
-    query(dc, 'SET @@repo1_head=Merge("master");')
-    query_and_test_results(dc, "SELECT * from dolt_conflicts;", [{"table": "test", "num_conflicts": "1"}])
-    resolve_theirs(dc)
-    expected = [row(0,1,0), row(1,1,1), row(2,2,2)]
-    query_and_test_results(dc, "SELECT pk, c1, c2 FROM test ORDER BY pk", expected)
-    commit_and_update_branch(dc, "resolved conflicts", ['HASHOF("HEAD^1")', 'HASHOF("HEAD^2")'], "master")
+        ctx['race_result'] = "loser"
+        create_branch(dc, "merge")
+        commit_and_update_branch(dc, "set c1 to 2", "@@repo1_head", "merge")
+        query(dc, 'SET @@repo1_head=Merge("master");')
+        query_and_test_results(dc, "SELECT * from dolt_conflicts;", [{"table": "test", "num_conflicts": "1"}])
+        resolve_theirs(dc)
+        expected = [row(0,their_val,0), row(1,1,1), row(2,2,2)]
+        query_and_test_results(dc, "SELECT pk, c1, c2 FROM test ORDER BY pk", expected)
+        #commit_and_update_branch(dc, "resolved conflicts", 'HASHOF("HEAD~1")', "master")
 
 
 # test script
 MAX_SIMULTANEOUS_CONNECTIONS = 2
 PORT_STR = sys.argv[1]
 
-CONNECTIONS = [None]*MAX_SIMULTANEOUS_CONNECTIONS
+class Session(object):
+    def __init__(self):
+       self.dc = DoltConnection(port=int(PORT_STR), database="repo1", user='dolt', auto_commit=False)
+       self.ctx = {}
+
+SESSIONS = [None]*MAX_SIMULTANEOUS_CONNECTIONS
 for i in range(MAX_SIMULTANEOUS_CONNECTIONS):
-    CONNECTIONS[i] = DoltConnection(port=int(PORT_STR), database="repo1", user='dolt', auto_commit=False)
+    SESSIONS[i] = Session()
 
 WORK_QUEUE = Queue()
 
 # work item run by workers
 class WorkItem(object):
-    def __init__(self, dc, *work_funcs):
-        self.dc = dc
+    def __init__(self, sess, *work_funcs):
+        self.session = sess
         self.work_funcs = work_funcs
         self.exception = None
 
@@ -160,7 +161,7 @@ def worker():
             item = WORK_QUEUE.get()
 
             for work_func in item.work_funcs:
-                work_func(item.dc)
+                work_func(item.session.dc, item.session.ctx)
 
             WORK_QUEUE.task_done()
         except Exception as e:
@@ -173,26 +174,48 @@ for i in range(MAX_SIMULTANEOUS_CONNECTIONS):
      t.daemon = True
      t.start()
 
+
+class Stage(object):
+    def __init__(self, work_items, state_validator):
+        self.work_items = work_items
+        self.state_validator = state_validator
+
+# stage validation
+def test_race_results():
+    winners = 0
+    losers = 0
+
+    for session in SESSIONS:
+        if session.ctx['race_result'] == "winner":
+            winners += 1
+        elif session.ctx['race_result'] == "loser":
+            losers += 1
+
+    if winners != 1 and losers != 1:
+        raise Exception("concurrency issue. Winners: %d Losers: %d" % (winners, losers))
+
 # This defines the actual test script.  Each stage in the script has a list of work items.  Each work item
 # in a stage should have a different connection associated with it.  Each connections work is done in parallel
 # each of the work functions for a connection is executed in order.
-work_item_stages = [
-    [WorkItem(CONNECTIONS[0], connect, create_tables)],
-    [WorkItem(CONNECTIONS[0], seed_master), WorkItem(CONNECTIONS[1], connect, duplicate_table_create)],
-    [WorkItem(CONNECTIONS[0], modify_pk0_on_master_and_commit), WorkItem(CONNECTIONS[1], modify_pk0_on_master_no_commit)],
-    [WorkItem(CONNECTIONS[1], fail_to_commit, commit_to_feature, merge_resolve_commit)]
+stages = [
+    Stage([WorkItem(SESSIONS[0], connect, create_tables)], None),
+    Stage([WorkItem(SESSIONS[0], seed_master), WorkItem(SESSIONS[1], connect, duplicate_table_create)], None),
+    Stage([WorkItem(SESSIONS[0], race_to_commit1), WorkItem(SESSIONS[1], race_to_commit2)], test_race_results),
 ]
 
 # Loop through the work item stages executing each stage by sending the work items for the stage to the worker threads
 # and then waiting for all of them to finish before moving on to the next one.  Checks for an error after every stage.
-for stage, work_items in enumerate(work_item_stages):
-    print("Running stage %d / %d" % (stage,len(work_item_stages)))
-    for work_item in work_items:
+for stage_num, stage in enumerate(stages):
+    print("Running stage %d / %d" % (stage_num, len(stages)))
+    for work_item in stage.work_items:
         WORK_QUEUE.put(work_item)
 
     WORK_QUEUE.join()
 
-    for work_item in work_items:
+    for work_item in stage.work_items:
         if work_item.exception is not None:
             print_err(work_item.exception)
             sys.exit(1)
+
+    if stage.state_validator is not None:
+        stage.state_validator()
